f0fca06b4d1b36a97941327a1c0edce5fcfb0d6d
Filippo Graziano <filippograziano13@gmail.com>
2026-05-27 14:42:04 +0200

    add on/off lamp

--- scrip.js
+++ scrip.js
@@ -1,4 +1,21 @@
-const img = document.querySelector(`img`)
-const button = document.querySelector(`button`)
+const img = document.querySelector(`img`);
+const button = document.querySelector(`button`);
 
-button.addEventListener(`click`, () => img.src = `img/yellow_lamp.png`)
+const offLamp = () => {
+
+    img.src = `img/white_lamp.png`
+    button.innerHTML = `Accendi`;
+    button.removeEventListener(`click`, offLamp);
+    console.log(`hai spento`)
+};
+
+const onLamp = () => {
+
+    img.src = `img/yellow_lamp.png`;
+    button.innerHTML = `Spegni`;
+    button.addEventListener(`click`, offLamp);
+    console.log(`hai acceso`)
+    
+};
+
+button.addEventListener(`click`, onLamp);
